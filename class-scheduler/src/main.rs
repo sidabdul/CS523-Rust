1c6c22e6a18199ed46a073393655714ca1c8ebac
sidabdul <sidali@pdx.edu>
2025-12-03 19:42:07 -0800

    added parser for days

--- class-scheduler/src/main.rs
+++ class-scheduler/src/main.rs
@@ -14,6 +14,43 @@ enum Day {
     Sun,
 }
 
+impl Day {
+    fn short(&self) -> &'static str {
+        match self {
+            Day::Mon => "Mon",
+            Day::Tue => "Tue",
+            Day::Wed => "Wed",
+            Day::Thu => "Thu",
+            Day::Fri => "Fri",
+            Day::Sat => "Sat",
+            Day::Sun => "Sun",
+        }
+    }
+    fn parse(s: &str) -> Option<Self> {
+        match s.to_ascii_lowercase().as_str() {
+            "mon" | "monday" => Some(Day::Mon),
+            "tue" | "tuesday" => Some(Day::Tue),
+            "wed" | "wednesday" => Some(Day::Wed),
+            "thu" | "thursday" => Some(Day::Thu),
+            "fri" | "friday" => Some(Day::Fri),
+            "sat" | "saturday" => Some(Day::Sat),
+            "sun" | "sunday" => Some(Day::Sun),
+            _ => None,
+        }
+    }
+    fn all() -> [Day; 7] {
+        [
+            Day::Mon,
+            Day::Tue,
+            Day::Wed,
+            Day::Thu,
+            Day::Fri,
+            Day::Sat,
+            Day::Sun,
+        ]
+    }
+}
+
 
 fn main() {
     println!("Hello, world!");
